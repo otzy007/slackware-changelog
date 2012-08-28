3da78cab1b2c9958d2499d6062b484052e331f94
Andrei G <otzy007>
2012-08-28 15:07:59 +0300

    rbp

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -1,7 +1,8 @@
 # The homepage controller
 class HomepageController < ApplicationController
   caches_page :about
-  
+
+  # Main Page
   def index
     # Check if there are new updates available
     ChangeLogController.new.index
