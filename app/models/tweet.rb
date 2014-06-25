4a30c64d85f9cebc137ca86134de1877eaeaaedc
Andrei Gliga <otzy_007@yahoo.com>
2014-06-25 12:48:34 +0300

    refactored the Tweet model to handle the new Twitter gem

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -5,7 +5,7 @@ class Tweet < ActiveRecord::Base
     # Load the YAML configuration
     twitter_config = YAML.load(File.read(Rails.root.join('config', 'twitter.yml')))[Rails.env]
     # credentials 
-    Twitter.configure do |config|
+    client = Twitter::REST::Client.new do |config|
       config.consumer_key = twitter_config["consumer_key#{version}"]
       config.consumer_secret = twitter_config["consumer_secret#{version}"]
       config.oauth_token = twitter_config["oauth_token#{version}"]
@@ -15,7 +15,7 @@ class Tweet < ActiveRecord::Base
     puts "tweeting...."
     # Status update
     begin
-      embed_code = Twitter.oembed(Twitter.update(tweet).id).html
+      embed_code = client.oembed(client.update(tweet)).html
       Tweet.first.update_attributes({"current#{version == 64 ? 64: ''}".to_sym => embed_code}, :without_protection => true)
 
     rescue Twitter::Error::Forbidden
